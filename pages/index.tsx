a38e4bac17b33d6b65182837414e6df456de0411
Found Pan Tiger <renzholy@gmail.com>
2021-02-07 21:16:13 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,32 @@
+import { useState } from 'react'
+import useSWR from 'swr'
+
 export default function Index() {
-  return null
+  const [keyword, setKeyword] = useState('')
+  const { data } = useSWR<{
+    currencies: {
+      id: string
+      name: string
+      symbol: string
+    }[]
+  }>(['search', 'coin', keyword], () =>
+    fetch(
+      `https://api.coinpaprika.com/v1/search/?q=${encodeURIComponent(keyword)}`,
+    ).then((response) => response.json()),
+  )
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={(e) => {
+          setKeyword(e.target.value)
+        }}
+        placeholder="search"
+      />
+      {data?.currencies.map((currency) => (
+        <div key={currency.id}>{currency.name}</div>
+      ))}
+    </div>
+  )
 }
