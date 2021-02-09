06fe3bd4e9322e8ccffa71f7c5722c5e1021f5d9
Found Pan Tiger <renzholy@gmail.com>
2021-02-09 16:27:48 +0800

    add icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,15 +10,43 @@ import {
   Intent,
   InputGroup,
 } from '@blueprintjs/core'
+import {
+  RiBitCoinLine,
+  RiExchangeLine,
+  RiFundsLine,
+  RiStockLine,
+} from 'react-icons/ri'
 
 import { Price } from '../components/price'
 import { useAllItems } from '../hooks/use-api'
 import { db } from '../libs/db'
-import { Asset } from '../libs/types'
-
-const splitter = '|'
+import { Asset, AssetType } from '../libs/types'
 
 const AssetSuggest = Suggest.ofType<Asset>()
+
+const iconClassName = css`
+  fill: #5c7080;
+  margin-top: 2px;
+  margin-right: 7px;
+`
+
+const iconLargeClassName = css`
+  fill: #5c7080;
+  margin-top: 3px;
+  margin-right: 7px;
+`
+
+function icons(type: AssetType, large?: boolean) {
+  const className = large ? iconLargeClassName : iconClassName
+  return {
+    [AssetType.FOREX]: <RiExchangeLine size={16} className={className} />,
+    [AssetType.CRYPTO]: <RiBitCoinLine size={16} className={className} />,
+    [AssetType.STOCK_CN]: <RiStockLine size={16} className={className} />,
+    [AssetType.STOCK_HK]: <RiStockLine size={16} className={className} />,
+    [AssetType.STOCK_US]: <RiStockLine size={16} className={className} />,
+    [AssetType.FUND]: <RiFundsLine size={16} className={className} />,
+  }[type]
+}
 
 export default function Index() {
   const [keyword, setKeyword] = useState('')
@@ -63,6 +91,7 @@ export default function Index() {
           inputValueRenderer={(item) => item.name}
           itemRenderer={(item, { handleClick, modifiers }) => (
             <MenuItem
+              icon={icons(item.type)}
               text={item.name}
               label={item.symbol}
               onClick={handleClick}
@@ -127,12 +156,13 @@ export default function Index() {
           )}>
           {list.map((item) => (
             <MenuItem
-              key={`${item.type}${splitter}${item.id}`}
+              key={item.type + item.id}
               onClick={() => {
                 setList((old) =>
                   old.filter((i) => i.type !== item.type || i.id !== item.id),
                 )
               }}
+              icon={icons(item.type, true)}
               text={
                 <Price amount={item.amount} type={item.type} id={item.id} />
               }
