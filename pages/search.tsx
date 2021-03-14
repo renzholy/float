29c92abb267e0c9fc66aab383da4250bb4a83893
RenzHoly <renzholy@gmail.com>
2021-03-14 16:56:56 +0800

    Update search.tsx

--- pages/search.tsx
+++ pages/search.tsx
@@ -3,46 +3,109 @@
 import { css, cx } from '@linaria/core'
 import { useState } from 'react'
 
+import { useSearch } from '../hooks/use-search'
+
 export default function Search() {
   const [keyword, setKeyword] = useState('')
+  const { data } = useSearch(keyword)
 
   return (
     <div
-      className={cx(
-        css`
+      className={css`
+        padding: 16px;
+      `}>
+      <div
+        className={css`
           display: flex;
-          font-size: 24px;
-          padding: 16px;
-        `,
-        'nes-field',
-      )}>
-      <input
-        type="text"
-        id="name_field"
-        className={cx(
-          'nes-input',
-          css`
-            outline: none;
-          `,
-        )}
-        placeholder="股票、基金、外汇、数字货币"
-        value={keyword}
-        onChange={(e) => {
-          setKeyword(e.target.value)
-        }}
-      />
-      <button
-        type="button"
-        className={cx(
-          'nes-btn',
-          keyword ? 'is-primary' : 'is-disabled',
-          css`
-            margin-left: 8px;
-            flex-shrink: 0;
-          `,
-        )}>
-        搜索
-      </button>
+          margin-bottom: 8px;
+        `}>
+        <input
+          type="text"
+          id="name_field"
+          className={cx(
+            'nes-input',
+            css`
+              outline: none;
+            `,
+          )}
+          placeholder="股票、基金、外汇、数字货币"
+          value={keyword}
+          onChange={(e) => {
+            setKeyword(e.target.value)
+          }}
+        />
+        <button
+          type="button"
+          className={cx(
+            'nes-btn',
+            keyword ? 'is-primary' : 'is-disabled',
+            css`
+              margin-left: 12px;
+              flex-shrink: 0;
+            `,
+          )}>
+          搜索
+        </button>
+      </div>
+      {data.length ? (
+        <div className="nes-table-responsive">
+          <table
+            className={cx(
+              'nes-table is-bordered',
+              css`
+                width: -webkit-fill-available;
+                & td {
+                  vertical-align: top;
+                }
+              `,
+            )}>
+            <thead>
+              <tr>
+                <th>名称</th>
+                <th>代码</th>
+                <th>类型</th>
+                <th
+                  className={css`
+                    white-space: nowrap;
+                  `}>
+                  操作
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr key={item.id + item.type}>
+                  <td>{item.name}</td>
+                  <td
+                    className={css`
+                      white-space: nowrap;
+                    `}>
+                    {item.label}
+                  </td>
+                  <td
+                    className={css`
+                      white-space: nowrap;
+                    `}>
+                    {item.type}
+                  </td>
+                  <td
+                    align="center"
+                    className={cx(
+                      css`
+                        &:hover {
+                          color: #209cee;
+                        }
+                      `,
+                      'nes-pointer',
+                    )}>
+                    +
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
     </div>
   )
 }
