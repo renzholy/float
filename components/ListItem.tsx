ef775e1ea86b51983ab1ce068123e02e358762e9
RenzHoly <renzholy@gmail.com>
2021-03-14 18:38:23 +0800

    update

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,20 +1,35 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
 import { css, cx } from '@linaria/core'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { usePrice } from '../hooks/use-price'
 import db from '../libs/db'
 import { formatNumber } from '../libs/formatter'
 import { Item } from '../libs/types'
 
-export function ListItem(props: { value: Item }) {
+export function ListItem(props: {
+  value: Item
+  isExpanded: boolean
+  onClick(): void
+}) {
   const item = props.value
   const { data: price } = usePrice('CNY', item.type, item.id)
   useEffect(() => {
     db.items.update([item.type, item.id], { price })
   }, [item.id, item.type, price])
+  const [amount, setAmount] = useState('')
+  const [cost, setCost] = useState('')
+  useEffect(() => {
+    setAmount(item.amount.toString())
+    if (item.cost !== undefined) {
+      setCost(item.cost.toString())
+    }
+  }, [item.amount, item.cost])
 
   return (
-    <tr>
+    <tr onClick={props.onClick}>
       <td>
         {item.name}
         {item.price === undefined ? null : (
@@ -29,19 +44,109 @@ export function ListItem(props: { value: Item }) {
           </span>
         )}
         <br />
-        <span className="nes-text is-disabled">
-          {item.type}
-          &nbsp;
-          {item.code}
-        </span>
-        <span
-          className={css`
-            float: right;
-          `}>
-          {item.price === undefined
-            ? '-'
-            : formatNumber(item.amount * item.price)}
-        </span>
+
+        {props.isExpanded ? (
+          <div
+            className={css`
+              margin-top: 8px;
+              display: flex;
+            `}
+            onClick={(e) => {
+              e.stopPropagation()
+            }}>
+            <input
+              type="text"
+              className={cx(
+                'nes-input',
+                amount && Number.isNaN(parseFloat(amount))
+                  ? 'is-error'
+                  : undefined,
+                css`
+                  outline: none;
+                `,
+              )}
+              placeholder="数量"
+              value={amount}
+              onChange={(e) => {
+                setAmount(e.target.value)
+              }}
+            />
+            <input
+              type="text"
+              className={cx(
+                'nes-input',
+                cost && Number.isNaN(parseFloat(cost)) ? 'is-error' : undefined,
+                css`
+                  outline: none;
+                  margin-left: 16px;
+                `,
+              )}
+              placeholder="成本"
+              value={cost}
+              onChange={(e) => {
+                setCost(e.target.value)
+              }}
+            />
+            <button
+              type="button"
+              className={cx(
+                css`
+                  margin-left: 16px;
+                  flex-shrink: 0;
+                `,
+                'nes-btn',
+                Number.isNaN(parseFloat(amount)) &&
+                  Number.isNaN(parseFloat(cost))
+                  ? 'is-disabled'
+                  : 'is-primary',
+              )}
+              onClick={async () => {
+                await db.items.update([item.type, item.id], {
+                  amount: Number.isNaN(parseFloat(amount))
+                    ? undefined
+                    : parseFloat(amount),
+                  cost: Number.isNaN(parseFloat(cost))
+                    ? undefined
+                    : parseFloat(cost),
+                })
+                props.onClick()
+              }}>
+              设置
+            </button>
+            <button
+              type="button"
+              className={cx(
+                css`
+                  margin-left: 16px;
+                  flex-shrink: 0;
+                `,
+                'nes-btn is-warning',
+              )}
+              onClick={async () => {
+                await db.items.delete([item.type, item.id])
+                props.onClick()
+              }}>
+              移除
+            </button>
+          </div>
+        ) : (
+          <>
+            <span className="nes-text is-disabled">
+              {item.type}
+              &nbsp;
+              {item.code}
+            </span>
+            <span
+              className={css`
+                float: right;
+              `}>
+              {item.price === undefined
+                ? '-'
+                : formatNumber(item.amount * item.price)}
+            </span>
+            <br />
+          </>
+        )}
       </td>
     </tr>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
 import { css, cx } from '@linaria/core'
 import { sumBy } from 'lodash'
 import { useRouter } from 'next/router'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import useSWR from 'swr'
 
 import { ListItem } from '../components/ListItem'
 import db from '../libs/db'
 import { formatNumber } from '../libs/formatter'
+import { ItemType } from '../libs/types'
 
 export default function Index() {
   const router = useRouter()
-  const { data: items } = useSWR('items', () => db.items.toArray(), {
-    refreshInterval: 2000,
-  })
+  const { data: items, revalidate } = useSWR(
+    'items',
+    () => db.items.toArray(),
+    {
+      refreshInterval: 2000,
+    },
+  )
   const total = useMemo(
     () =>
       sumBy(items, (item) =>
@@ -20,6 +25,7 @@ export default function Index() {
       ),
     [items],
   )
+  const [expanded, setExpanded] = useState<[ItemType, string]>()
 
   return (
     <div
@@ -57,7 +63,21 @@ export default function Index() {
             )}>
             <tbody>
               {items?.map((item) => (
-                <ListItem key={item.type + item.id} value={item} />
+                <ListItem
+                  key={item.type + item.id}
+                  value={item}
+                  isExpanded={
+                    item.type === expanded?.[0] && item.id === expanded[1]
+                  }
+                  onClick={() => {
+                    setExpanded(
+                      item.type === expanded?.[0] && item.id === expanded[1]
+                        ? undefined
+                        : [item.type, item.id],
+                    )
+                    revalidate()
+                  }}
+                />
               ))}
             </tbody>
           </table>
@@ -69,7 +89,7 @@ export default function Index() {
           css`
             margin-top: 16px;
           `,
-          'nes-btn is-primary',
+          'nes-btn is-success',
         )}
         onClick={() => {
           router.push('/search')
