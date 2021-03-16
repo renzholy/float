aeff3c70cc7fc2a74c3e1e70997d91f52a1c254b
RenzHoly <renzholy@gmail.com>
2021-03-16 18:20:36 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Float - 浮动收益</title>
+        <meta
+          name="description"
+          content="随时随地查看浮动收益，支持股票、基金、外汇、加密货币。"
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
