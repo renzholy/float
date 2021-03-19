25874e1e2aa223e78f7119f94f037d64c0f7e948
RenzHoly <renzholy@gmail.com>
2021-03-19 20:18:59 +0800

    update deps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable jsx-a11y/control-has-associated-label */
 
 import { css, cx } from '@linaria/core'
 import orderBy from 'lodash/orderBy'
